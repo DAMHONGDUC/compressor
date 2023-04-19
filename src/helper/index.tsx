a030782b2beec77bf0b2e775faf9cba4892c8c73
Dam Hong Duc <hongduc001h@gmail.com>
2023-04-19 15:49:50 +0700

    add some lib

--- src/helper/index.tsx
+++ src/helper/index.tsx
@@ -20,3 +20,79 @@ export const handlePermission = async () => {
     );
   }
 };
+
+// export const compressFile = async (
+//   uriInput: string,
+//   name: string,
+//   type: string,
+// ) => {
+//   if (isImage(type)) return compressImage(uriInput, name);
+//   else return uriInput;
+// };
+
+// const compressImage = async (uriInput: string, name: string) => {
+//   try {
+//     let imageResizeConfig = store.getState().auth.userData
+//       ?.imageResizeConfig ?? {
+//       maxWidth: IMAGE_UPLOAD_MAX_WIDTH,
+//       maxHeight: IMAGE_UPLOAD_MAX_HEIGHT,
+//       quality: IMAGE_UPLOAD_QUALITY,
+//       sizeToTrigger: SIZE_TO_TRIGGER,
+//     };
+//     const imageCompressOption = {
+//       maxWidth:
+//         imageResizeConfig.maxWidth === -1
+//           ? IMAGE_UPLOAD_MAX_WIDTH
+//           : imageResizeConfig.maxWidth,
+//       maxHeight:
+//         imageResizeConfig.maxHeight === -1
+//           ? IMAGE_UPLOAD_MAX_HEIGHT
+//           : imageResizeConfig.maxHeight,
+//       quality: imageResizeConfig.quality,
+//     };
+
+//     const isAndroid = Platform.OS === 'android';
+
+//     let uri = isAndroid ? uriInput : uriInput.replace('file://', '');
+//     if (isAndroid && uri.includes('com.android.providers.downloads')) {
+//       uri = `${RNFetchBlob.fs.dirs.DownloadDir}/${name}`;
+//     }
+
+//     const stat = await RNFetchBlob.fs.stat(
+//       isAndroid ? uri : uri.replace('file://', ''),
+//     );
+
+//     if (stat.size > imageResizeConfig.sizeToTrigger) {
+//       const cacheName = `${name}_${Date.now()}`;
+//       const destPath = `${RNFetchBlob.fs.dirs.CacheDir}/${cacheName}`;
+//       store.dispatch(addListCacheImageCompress(cacheName));
+
+//       await RNFetchBlob.fs.cp(uri, destPath);
+
+//       console.log(uri);
+//       const result = await ImageCompress.compress(
+//         destPath,
+//         imageCompressOption,
+//       );
+
+//       return result;
+//     }
+//     return uriInput;
+//   } catch (err) {}
+// };
+
+// export const deleteCompressedCache = async () => {
+//   try {
+//     const listCacheImageCompress =
+//       store.getState().appInfo.listCacheImageCompress;
+
+//     await Promise.all(
+//       listCacheImageCompress.map(async e => {
+//         const destPath = `${RNFetchBlob.fs.dirs.CacheDir}/${e}`;
+//         const destPathExist = await RNFetchBlob.fs.exists(destPath);
+
+//         if (destPathExist) await RNFetchBlob.fs.unlink(destPath);
+//       }),
+//     );
+//   } catch (err) {}
+// };
--- src/screen/home/home-screen.tsx
+++ src/screen/home/home-screen.tsx
@@ -1,26 +1,32 @@
-import {
-  AspectRatio,
-  Heading,
-  Text,
-  Box,
-  Image,
-  Center,
-  Stack,
-  HStack,
-  Container,
-  IconButton,
-  Icon,
-  VStack,
-  Row,
-  Column,
-} from 'native-base';
-import Feather from 'react-native-vector-icons/Feather';
+import React from 'react';
+import {Box, Column, VStack, Button} from 'native-base';
+import DocumentPicker from 'react-native-document-picker';
+import {Image as ImageCompress} from 'react-native-compressor';
 
 export default function HomeScreen() {
+  const pickDocument = async () => {
+    try {
+      const document = await DocumentPicker.pickMultiple({
+        type: [DocumentPicker.types.images, DocumentPicker.types.video],
+      });
+
+      console.log(document[0].uri);
+
+      const result = await ImageCompress.compress(document[0].uri, {
+        maxWidth: 1000,
+        quality: 0.8,
+      });
+
+      console.log({result});
+    } catch (err) {
+      console.log({err});
+    }
+  };
+
   return (
     <Box backgroundColor={'primary.50'} flex={1} padding={3}>
       <Column>
-        <Row justifyContent={'space-between'}>
+        {/* <Row justifyContent={'space-between'}>
           <Heading>
             <Text color="rose.500"> Compressor</Text>
           </Heading>
@@ -32,7 +38,15 @@ export default function HomeScreen() {
             }}
             _pressed={{}}
           />
-        </Row>
+        </Row> */}
+        <VStack space={4} alignItems="center">
+          <Button
+            colorScheme="secondary"
+            size={'md'}
+            onPress={() => pickDocument()}>
+            Document Picker
+          </Button>
+        </VStack>
       </Column>
     </Box>
   );
